8058c53d86ec4b12e621f13d430af01c7cfe171b
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-06-06 10:20:03 -0500

    Reorganized the file structure to remove the example blog post from my blogs

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Garrett McGiffert';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'gmoney takes';
+    : 'GMoney takes';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
