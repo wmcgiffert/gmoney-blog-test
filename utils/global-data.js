17e08956c3e7585c4726b3c7a58d66ea114c2d0b
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-06-13 16:40:17 -0500

    Blog post 6-13-23

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Garrett McGiffert';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'GMoney takes';
+    : 'GMoney Takes';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
