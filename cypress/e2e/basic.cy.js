75474d637bfdd104338b49ce218a0b94d7d5ede3
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-06-20 11:34:23 -0500

    Blog post 6-20-23

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,7 +4,6 @@ describe('sample render test', () => {
   });
 
   it('displays the title text', () => {
-    // just need to check that cy.get('h1') contains any text
     cy.get('h1').contains('GMoney Takes');
   });
 });
